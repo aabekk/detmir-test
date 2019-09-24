3c88491b8acb0998e5d15af1a4fab1764bd4b752
Scofield001 <scofield-001@mail.ru>
2019-09-24 22:38:42 +0300

    Исправлен путь до картинок

--- gulpfile.js
+++ gulpfile.js
@@ -52,6 +52,16 @@ function svg() {
                 pretty: true
             }
         }))
+        // remove all fill, style and stroke declarations in out shapes
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        // cheerio plugin create unnecessary string '&gt;', so replace it.
         .pipe(replace('&gt;', '>'))
         // build svg sprite
         .pipe(svgSprite({
